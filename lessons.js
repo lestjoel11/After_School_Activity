3fc4dbf34efe588b829d06604ba2f46f2422d5c2
Lester Joel <lestjoel@gmail.com>
2020-11-06 20:44:18 +0400

    Add data to lessons array

---
+++ lessons.js
@@ -0,0 +1,79 @@
+let lessons=[
+    {
+    subject: "Math",
+    location: "Dubai",
+    price: "AED 350",
+    space: "5",
+    img
+    },
+    {
+    subject: "English",
+    location: "Abu Dhabi",
+    price: "AED 350",
+    space: "5",
+    img
+    },
+    {
+    subject: "Arabic",
+    location: "Dubai",
+    price: "AED 350",
+    space: "5",
+    img
+    },
+    {
+    subject: "Music",
+    location: "Sharjah",
+    price: "AED 350",
+    space: "5",
+    img
+    },
+    {
+    subject: "Art",
+    location: "Dubai",
+    price: "AED 350",
+    space: "5",
+    img
+    },
+    {
+    subject: "Computers",
+    location: "Dubai",
+    price: "AED 350",
+    space: "5",
+    img
+    },
+    {
+    subject: "Science",
+    location: "Abu Dhabi",
+    price: "AED 350",
+    space: "5",
+    img
+    },
+    {
+    subject: "Accountancy",
+    location: "Dubai",
+    price: "AED 350",
+    space: "5",
+    img
+    },
+    {
+    subject: "Biology",
+    location: "Dubai",
+    price: "AED 350",
+    space: "5",
+    img
+    },
+    {
+    subject: "Physics",
+    location: "Dubai",
+    price: "AED 350",
+    space: "5",
+    img
+    },
+    {
+    subject: "Chemistry",
+    location: "Dubai",
+    price: "AED 350",
+    space: "5",
+    img
+    }
+]
