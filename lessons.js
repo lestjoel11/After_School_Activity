9e79eb989cee23a258e9c5fa7f9e0796ff8ac4be
Lester Joel <lestjoel@gmail.com>
2020-11-08 00:42:03 +0400

    Add images for lessons object

--- lessons.js
+++ lessons.js
@@ -1,79 +1,82 @@
-let lessons=[
+let lessons = [
     {
+    id:1,
     subject: "Math",
     location: "Dubai",
     price: "AED 350",
     space: "5",
-    img
+    img:"img/math.jpg"
     },
     {
+    id:2,
     subject: "English",
     location: "Abu Dhabi",
     price: "AED 350",
     space: "5",
-    img
+    img:"img/english.jpg"
     },
     {
+    id:3,
     subject: "Arabic",
     location: "Dubai",
     price: "AED 350",
     space: "5",
-    img
+    img:"img/arabic.jpg"
     },
     {
+    id:4,
     subject: "Music",
     location: "Sharjah",
     price: "AED 350",
     space: "5",
-    img
+    img:"img/music.jpg"
     },
     {
+    id:5,
     subject: "Art",
     location: "Dubai",
     price: "AED 350",
     space: "5",
-    img
+    img:"img/art.jpg"
     },
     {
+    id:6,
     subject: "Computers",
     location: "Dubai",
     price: "AED 350",
     space: "5",
-    img
+    img:"img/computer.jpg"
     },
     {
+    id:7,
     subject: "Science",
     location: "Abu Dhabi",
     price: "AED 350",
     space: "5",
-    img
+    img:"img/science.jpg"
     },
     {
+    id:8,
     subject: "Accountancy",
     location: "Dubai",
     price: "AED 350",
     space: "5",
-    img
+    img:"img/accountancy.jpg"
     },
     {
+    id:9,
     subject: "Biology",
     location: "Dubai",
     price: "AED 350",
     space: "5",
-    img
+    img:"img/biology.jpg"
     },
     {
+    id:10,
     subject: "Physics",
     location: "Dubai",
     price: "AED 350",
     space: "5",
-    img
+    img:"img/physics.jpg"
     },
-    {
-    subject: "Chemistry",
-    location: "Dubai",
-    price: "AED 350",
-    space: "5",
-    img
-    }
 ]
